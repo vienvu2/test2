05e7bcaa2b6f8494ca2dffb615d1972711cc1983
VienBit <vienvu@bitelle.com>
2023-07-17 15:00:49 +0700

    12

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,18 +4,20 @@ import { getStoryblokApi } from "@storyblok/react/rsc";
 export default async function About() {
   const { data } = await fetchData();
 
-  console.log('About', data.story.content)
-  return (
-    <div>
-    {JSON.stringify(data.story.content)}
-      <StoryblokStory story={data.story} />
-    </div>
-  );
+  console.log("About", data);
+  if (data) {
+    return (
+      <div>
+        {JSON.stringify(data.story.content)}
+        <StoryblokStory story={data.story} />
+      </div>
+    );
+  }
+  return <div />;
 }
 
 export async function fetchData() {
   let sbParams = {};
-
   const storyblokApi = getStoryblokApi();
   return storyblokApi?.get(`cdn/stories/about`, sbParams);
 }
